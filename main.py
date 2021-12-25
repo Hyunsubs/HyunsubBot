088c8977fe5a5d0240a276bce43469641c536e95
Hyunsubs <49810954+Hyunsubs@users.noreply.github.com>
2021-12-25 13:54:22 +0900

    PLZBEFINAL
    PLEAAAAASE

--- main.py
+++ main.py
@@ -51,8 +51,9 @@ async def on_message(message):
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--no-sandbox")
         
+        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
         
-        driver = webdriver.Chrome(executable_path = "./app/main.py", chrome_options = chrome_options)
         driver.get(url)
         soup = bs(driver.page_source, "html.parser")
         driver.close()
